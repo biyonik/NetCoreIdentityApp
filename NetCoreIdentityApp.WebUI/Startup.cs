4bf09dcaa28f9fa1ade11e2c61c4a5de9c37c9c4
Ahmet Altun <ahmet.altun60@hotmail.com>
2020-12-13 21:44:33 +0300

    Connection String tanımlandı ve Startup içinde gerekli ayarlamalar yapıldı

--- NetCoreIdentityApp.WebUI/Startup.cs
+++ NetCoreIdentityApp.WebUI/Startup.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetCoreIdentityApp.WebUI.Context;
+using NetCoreIdentityApp.WebUI.Models;
 
 namespace NetCoreIdentityApp.WebUI
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options =>
@@ -13,6 +22,12 @@ namespace NetCoreIdentityApp.WebUI
                 options.EnableEndpointRouting = false;
             });
 
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(_configuration["ConnectionStrings:default"]);
+            });
+            services.AddIdentity<AppUser, AppRole>()
+                    .AddEntityFrameworkStores<AppIdentityDbContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
